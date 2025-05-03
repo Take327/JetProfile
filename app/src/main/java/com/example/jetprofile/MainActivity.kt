a82191581f9c6d56e27ad7e3a8b1ec2555248ee2
Take327 <gatewayseed2@gmail.com>
2025-05-04 03:44:57 +0900

    ネスト分割

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,67 +39,72 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),                      // 画面全体を占有
                     color = MaterialTheme.colors.background                 // テーマで定義された背景色を使用
                 ) {
-                    // Column: 子コンポーザブルを縦方向に並べるレイアウト
-                    // horizontalAlignment = Alignment.CenterHorizontally で列内を横方向に中央揃え
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(20.dp)
-                    ) {
-                        //プロフィール画像
-                        Image(
-                            painter = painterResource(id = R.drawable.profile),
-                            contentDescription = "プロフィール",
-                            modifier = Modifier
-                                .size(100.dp)
-                                .clip(RoundedCornerShape(10.dp))
-                        )
-                        // Spacer: 空白領域を挿入。ここでは下方向に20dpの余白を確保
-                        Spacer(
-                            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
-                        )
-                        // 1行目のテキスト
-                        Text(
-                            text = "Take",                         // 表示する文字列
-                            color = Color.Gray,                    // テキストの色をグレーに
-                            fontSize = 16.sp,                      // フォントサイズを16spに（sp は文字に最適化された単位）
-                            fontWeight = FontWeight.ExtraBold      // 太字の中でも極太を指定
-                        )
-
-                        // Spacer: 空白領域を挿入。ここでは下方向に20dpの余白を確保
-                        Spacer(
-                            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
-                        )
-
-                        // 2行目のテキスト
-                        Text(
-                            text = "職業：エンジニア",             // 日本語テキストも同様に表示可能
-                            color = Color.Gray,                    // 色は先と同じグレー
-                            fontSize = 16.sp,                      // サイズも揃える
-                            fontWeight = FontWeight.ExtraBold      // 太さも揃える
-                        )
-
-                        Spacer(
-                            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
-                        )
-                        // 会社情報
-                        CompanySection()
-                        // 詳細ボタン
-                        Button(
-                            onClick = { print("クリックされました") },
-                            modifier = Modifier.fillMaxWidth(),
-                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A))
-                        ) {
-                            Text(text = "詳細を表示", color = Color.White)
-                        }
-                        Spacer(
-                            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
-                        )
-                        //居住地　趣味
-                        DetailSection()
-                    }
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    // Column: 子コンポーザブルを縦方向に並べるレイアウト
+    // horizontalAlignment = Alignment.CenterHorizontally で列内を横方向に中央揃え
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(20.dp)
+    ) {
+        //プロフィール画像
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "プロフィール",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        // Spacer: 空白領域を挿入。ここでは下方向に20dpの余白を確保
+        Spacer(
+            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
+        )
+        // 1行目のテキスト
+        Text(
+            text = "Take",                         // 表示する文字列
+            color = Color.Gray,                    // テキストの色をグレーに
+            fontSize = 16.sp,                      // フォントサイズを16spに（sp は文字に最適化された単位）
+            fontWeight = FontWeight.ExtraBold      // 太字の中でも極太を指定
+        )
+
+        // Spacer: 空白領域を挿入。ここでは下方向に20dpの余白を確保
+        Spacer(
+            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
+        )
+
+        // 2行目のテキスト
+        Text(
+            text = "職業：エンジニア",             // 日本語テキストも同様に表示可能
+            color = Color.Gray,                    // 色は先と同じグレー
+            fontSize = 16.sp,                      // サイズも揃える
+            fontWeight = FontWeight.ExtraBold      // 太さも揃える
+        )
+
+        Spacer(
+            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
+        )
+        // 会社情報
+        CompanySection()
+        // 詳細ボタン
+        Button(
+            onClick = { print("クリックされました") },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A))
+        ) {
+            Text(text = "詳細を表示", color = Color.White)
+        }
+        Spacer(
+            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
+        )
+        //居住地　趣味
+        DetailSection()
     }
 }
 
