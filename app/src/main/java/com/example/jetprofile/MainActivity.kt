4b50e61f23571f7cee87d3113874b54ebac3806e
Take327 <gatewayseed2@gmail.com>
2025-05-04 02:51:31 +0900

    レイアウト完成

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -104,17 +109,7 @@ class MainActivity : ComponentActivity() {
                                 modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
                             )
                             //Email
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Icon(
-                                    imageVector = Icons.Default.Email,
-                                    contentDescription = "email",
-                                )
-                                Spacer(
-                                    modifier = Modifier.width(10.dp)      // dp は画面の密度に依存しないサイズ単位
-                                )
-                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
-
-                            }
+                            Label(icon = Icons.Default.Email, text = "Email")
                             Spacer(
                                 modifier = Modifier.height(10.dp)      // dp は画面の密度に依存しないサイズ単位
                             )
@@ -127,12 +122,61 @@ class MainActivity : ComponentActivity() {
                                 thickness = 2.dp,
                             )
                         }
+                        // 詳細ボタン
+                        Button(
+                            onClick = { print("クリックされました") },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A))
+                        ) {
+                            Text(text = "詳細を表示", color = Color.White)
+                        }
+                        Spacer(
+                            modifier = Modifier.height(20.dp)      // dp は画面の密度に依存しないサイズ単位
+                        )
+                        //居住地　趣味
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "プログラミング",
+                                color = Color.Gray
+                            )
+                            Spacer(
+                                modifier = Modifier.height(10.dp)      // dp は画面の密度に依存しないサイズ単位
+                            )
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地：神奈川県",
+                                color = Color.Gray
+                            )
+                        }
+
                     }
 
                 }
             }
         }
+
     }
 
 }
 
+@Composable
+fun Label(icon: ImageVector, text: String, color: Color = MaterialTheme.colors.onBackground) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+        )
+        Spacer(
+            modifier = Modifier.width(10.dp)      // dp は画面の密度に依存しないサイズ単位
+        )
+        Text(text = text, color = color, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+
+    }
+}
